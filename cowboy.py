f5ca0628025e095a2fca9482371a98b2c12060e9
Nelson Chen <crazysim@gmail.com>
2012-07-28 22:46:26 -0700

    ucsb as a city

--- cowboy.py
+++ cowboy.py
@@ -1,4 +1,5 @@
 import astral
 import praw
 
-print("I'm cowboy")
+ucsb = astral.City(('University of California, Santa Barbara Main Campus',
+                    'USA', 34.41254, -119.84813, 'US/Pacific'))
