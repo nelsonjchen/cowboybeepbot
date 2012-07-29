ffbd40a2fe043f593ca60279e74fa68e8522e0ef
Nelson Chen <crazysim@gmail.com>
2012-07-28 22:50:31 -0700

    night and day

--- cowboy.py
+++ cowboy.py
@@ -3,3 +3,8 @@ import praw
 
 ucsb = astral.City(('University of California, Santa Barbara Main Campus',
                     'USA', 34.41254, -119.84813, 'US/Pacific'))
+
+if (ucsb.solar_elevation() > 0):
+    print("day!")
+else:
+    print("night!")
