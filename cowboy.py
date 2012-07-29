e25c1d3a441969f8c2801ba262200d39b8a94521
Nelson Chen <crazysim@gmail.com>
2012-07-28 23:33:48 -0700

    use variables

--- cowboy.py
+++ cowboy.py
@@ -5,6 +5,11 @@ import os
 ucsb = astral.City(('University of California, Santa Barbara Main Campus',
                     'USA', 34.41254, -119.84813, 'US/Pacific'))
 
+user = os.environ['REDDIT_USER']
+password = os.environ['REDDIT_PASS']
+
+r = praw.Reddit("UCSB Subreddit Style Bot aka. CowboyBeepBopBot")
+
 if (ucsb.solar_elevation() > 0):
     print("day!")
 else:
